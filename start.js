1ee76b55f848b4379f385edeb98eb3ba34edd2f1
Jon Bardin <diclophis@gmail.com>
2015-12-31 13:18:55 -0800

    first working version

--- start.js
+++ start.js
@@ -28,23 +28,39 @@ noble.on('discover', function(peripheral) {
    
       bb8.connect(function() {
         console.log("connected");
-
+        var direction = 90;
+        var directionIntended = false;
         console.log("::START CALIBRATION::");
         bb8.startCalibration();
         setTimeout(function() {
           console.log("::FINISH CALIBRATION::");
           bb8.finishCalibration();
           // roll BB-8 in a random direction, changing direction every second 
-          setInterval(function() {
-            bb8.color({ red: 255, green: 0, blue: 255 });
+          var randomDir = function() {
+            if (true || Math.random() > 0.5) {
+              bb8.color({ red: (Math.random() * 255), green: 128, blue: 128 });
+            } else {
+              bb8.color({ red: 128, green: (Math.random() * 255), blue: 128 });
+            }
             console.log("random location");
-            var direction = Math.floor(Math.random() * 360);
-            bb8.roll(2000, direction);
-          }, 5000);
-        }, 5000);
+            //var direction = Math.floor(Math.random() * 360);
+            var cmdTimeout = 300.0;
+            if (directionIntended) {
+              //direction += parseInt(360.0 * (cmdTimeout / 1000.0));
+            } else {
+              //direction -= parseInt(360.0 * (cmdTimeout / 1000.0));
+            }
+            if (Math.random() > 0.5) {
+              directionIntended = !directionIntended;
+              direction += 100;
+            }
+            console.log(direction);
+            direction = Math.abs(direction) % 360;
+            bb8.roll(500, direction, function(rollState) { console.log("rolling", rollState); setTimeout(randomDir, cmdTimeout); });
+          };
+          randomDir();
+        }, 2000);
       });
     }
   }
 });
-
-
